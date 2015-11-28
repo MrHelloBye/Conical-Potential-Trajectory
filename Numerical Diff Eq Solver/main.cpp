6e86b6b9c85d6e6596dc39f658932cbc288e2e61
Liam Clink <mrhellobye@gmail.com>
2015-11-28 14:49:55 -0500

    Initial Commit

---
+++ Numerical Diff Eq Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Numerical Diff Eq Solver
+//
+//  Created by Liam Clink on 11/28/15.
+//  Copyright © 2015 Liam Clink. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
